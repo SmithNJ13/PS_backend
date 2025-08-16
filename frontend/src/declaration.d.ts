0464ab9ffc7e1398f36ce8745f718946a9fa9cdf
Nathan <136971822+SmithNJ13@users.noreply.github.com>
2025-08-16 04:42:16 +0100

    Delete frontend directory

--- frontend/src/declaration.d.ts
+++
@@ -1,8 +0,0 @@
-
-declare module "../../consts/api" {
-    export const baseURL: string;
-  }
-  
-  declare module "../../components/export" {
-    export { default as TeamBanner } from "./TeamBanner/index.jsx";
-}
--- frontend/src/pages/NotFound/index.jsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-  return (
-    <h1>BRRR! Error! Page NOT Found!</h1>
-  )
-}
-
-export default NotFound
