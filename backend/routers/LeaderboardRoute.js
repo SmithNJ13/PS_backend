53c12e51ea4d41460dae75babd7e3dd5edb365d4
Nathan <136971822+SmithNJ13@users.noreply.github.com>
2025-08-16 04:44:54 +0100

    Delete backend directory

--- backend/routers/LeaderboardRoute.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-const LeaderboardController = require("../controllers/leaderboard")
-
-const LeaderboardRouter = Router()
-
-LeaderboardRouter.get("/", LeaderboardController.index)
-
-module.exports = LeaderboardRouter
-
